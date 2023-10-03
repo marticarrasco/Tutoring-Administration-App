33c4ad4301cb261a13737c5b63ee5193e0cde637
Martí Carrasco <89729992+marticarrasco@users.noreply.github.com>
2023-10-03 12:02:23 +0200

    Bug Fix and new feautures regarding Mark Paid

--- app.py
+++ app.py
@@ -76,14 +76,13 @@ def refresh_sessions_list():
 
     conn.close()
 
-
 def delete_session():
     """Delete the selected session from the database."""
     selected_items = sessions_tree.selection()
     if not selected_items:
         return
 
-    student_name, session_date, _ = sessions_tree.item(selected_items[0])['values']
+    student_name, session_date, _, __ = sessions_tree.item(selected_items[0])['values']
     session_date_db_format = datetime.strptime(session_date, '%d-%m-%Y').strftime('%Y-%m-%d')  # Convert to database format
     
     # Confirm deletion
@@ -111,21 +110,22 @@ def delete_session():
     
     refresh_sessions_list()
 
-
 def edit_session():
     """Edit the selected session's details."""
     selected_items = sessions_tree.selection()
     if not selected_items:
         return
 
-    session_date, hours = sessions_tree.item(selected_items[0])['values'][1:]
+    session_date, hours, is_paid = sessions_tree.item(selected_items[0])['values'][1:]
     session_date_db_format = datetime.strptime(session_date, '%d-%m-%Y').strftime('%Y-%m-%d')  # Convert to database format
-    d = EditSessionDialog(root, title="Edit Session", initial_data=(session_date, hours))
+    d = EditSessionDialog(root, title="Edit Session", initial_data=(session_date, hours, is_paid))
     
     # If the user provided new data, update the database
     if d.result:
-        new_date, new_hours = d.result
+        new_date, new_hours, new_is_paid = d.result
         new_date_db_format = datetime.strptime(new_date, '%d-%m-%Y').strftime('%Y-%m-%d')  # Convert new date to database format
+        new_is_paid_int = 1 if new_is_paid == "Yes" else 0  # Convert "Yes"/"No" to 1/0
+
         try:
             new_hours = float(new_hours)
             student_name = sessions_tree.item(selected_items[0])['values'][0]
@@ -134,12 +134,12 @@ def edit_session():
             
             query = """
             UPDATE TutoringSessions
-            SET date_of_session = ?, hours_worked = ?
+            SET date_of_session = ?, hours_worked = ?, is_paid = ?
             WHERE student_id = (SELECT id FROM Students WHERE name = ?)
             AND date_of_session = ?
             """
             
-            cursor.execute(query, (new_date_db_format, new_hours, student_name, session_date_db_format))
+            cursor.execute(query, (new_date_db_format, new_hours, new_is_paid_int, student_name, session_date_db_format))
             conn.commit()
             conn.close()
             refresh_sessions_list()
@@ -149,6 +149,38 @@ def edit_session():
         except ValueError:
             messagebox.showerror("Error", "Please enter a valid number for hours.")
 
+def mark_as_paid():
+    selected_items = sessions_tree.selection()
+    if not selected_items:
+        return
+    student_name, session_date, _, _ = sessions_tree.item(selected_items[0])['values']
+    session_date_db_format = datetime.strptime(session_date, '%d-%m-%Y').strftime('%Y-%m-%d')  # Convert to database format
+    
+    conn = sqlite3.connect('tutoring_app.db')
+    cursor = conn.cursor()
+    
+    cursor.execute("UPDATE TutoringSessions SET is_paid=1 WHERE student_id = (SELECT id FROM Students WHERE name = ?) AND date_of_session = ?", (student_name, session_date_db_format))
+    conn.commit()
+    conn.close()
+    
+    refresh_sessions_list()
+
+def mark_as_unpaid():
+    # similar to mark_as_paid, but set is_paid=0
+    selected_items = sessions_tree.selection()
+    if not selected_items:
+        return
+    student_name, session_date, _, _ = sessions_tree.item(selected_items[0])['values']
+    session_date_db_format = datetime.strptime(session_date, '%d-%m-%Y').strftime('%Y-%m-%d')  # Convert to database format
+    
+    conn = sqlite3.connect('tutoring_app.db')
+    cursor = conn.cursor()
+    
+    cursor.execute("UPDATE TutoringSessions SET is_paid=0 WHERE student_id = (SELECT id FROM Students WHERE name = ?) AND date_of_session = ?", (student_name, session_date_db_format))
+    conn.commit()
+    conn.close()
+    
+    refresh_sessions_list()
 
 def refresh_students_list():
     """Refresh the students combobox and treeview from the database."""
@@ -218,6 +250,7 @@ def edit_student():
             conn.commit()
             conn.close()
             refresh_students_list()
+            refresh_sessions_list()
         except ValueError:
             messagebox.showerror("Error", "Please enter a valid hourly rate.")
 
@@ -272,6 +305,7 @@ class EditSessionDialog(Dialog):
     def body(self, master):
         ttk.Label(master, width=label_width, text="Date:").grid(row=0)
         ttk.Label(master, width=label_width, text="Hours:").grid(row=1)
+        ttk.Label(master, width=label_width, text="Is Paid?").grid(row=2)
 
         self.date_entry = ttk.Entry(master, width=entry_width)
         self.date_entry.grid(row=0, column=1)
@@ -281,19 +315,28 @@ class EditSessionDialog(Dialog):
         self.hours_entry.grid(row=1, column=1)
         self.hours_entry.insert(0, self.init_data[1])
 
+        self.is_paid_var = tk.IntVar()
+        # Set the initial value of the Checkbutton based on "Yes" or "No"
+        if self.init_data[2] == "Yes":
+            self.is_paid_var.set(1)
+        else:
+            self.is_paid_var.set(0)
+        ttk.Checkbutton(master, variable=self.is_paid_var, text="Paid").grid(row=2, column=1)
+
         return self.date_entry  # initial focus
 
     def apply(self):
-        self.result = (self.date_entry.get(), self.hours_entry.get())
+        self.result = (self.date_entry.get(), self.hours_entry.get(), "Yes" if self.is_paid_var.get() == 1 else "No")
 
-    def __init__(self, parent, title=None, initial_data=("", "")):
+    def __init__(self, parent, title=None, initial_data=("", "", "No")):
         self.init_data = initial_data
         super().__init__(parent, title)
+
     
 # Main window setup
 root = tk.Tk()
 root.title("Tutoring Administration App")
-#root.state('zoomed')
+root.resizable(False, False)
 
 # Frame for manual session entry
 frame_manual_entry = ttk.LabelFrame(root, text="Manual Session Entry", padding=(10, 5))
@@ -317,6 +360,7 @@ lbl_hours = ttk.Label(frame_manual_entry, width=label_width, text="Hours:")
 lbl_hours.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
 
 hours_entry = ttk.Entry(frame_manual_entry, width=entry_width+1)
+hours_entry.insert(0, "1")
 hours_entry.grid(row=2, column=1, padx=5, pady=5)
 
 btn_add_session = ttk.Button(frame_manual_entry, text="Add Session", command=add_manual_session)
@@ -332,92 +376,81 @@ sessions_tree.heading('Student', text='Student')
 sessions_tree.heading('Date', text='Date')
 sessions_tree.heading('Hours', text='Hours')
 sessions_tree.heading('Paid', text='Paid')  # New heading for payment status
-sessions_tree.pack(padx=5, pady=5, fill=tk.BOTH, expand=True)
-
+sessions_tree.grid(row=0, column=0, columnspan=4, padx=5, pady=5, sticky='nsew')
 
 refresh_sessions_list()
 
-def mark_as_paid():
-    selected_items = sessions_tree.selection()
-    if not selected_items:
-        return
-    student_name, session_date, _, _ = sessions_tree.item(selected_items[0])['values']
-    session_date_db_format = datetime.strptime(session_date, '%d-%m-%Y').strftime('%Y-%m-%d')  # Convert to database format
-    
-    conn = sqlite3.connect('tutoring_app.db')
-    cursor = conn.cursor()
-    
-    cursor.execute("UPDATE TutoringSessions SET is_paid=1 WHERE student_id = (SELECT id FROM Students WHERE name = ?) AND date_of_session = ?", (student_name, session_date_db_format))
-    conn.commit()
-    conn.close()
-    
-    refresh_sessions_list()
+# Frame to hold the buttons
+btn_frame = ttk.Frame(frame_sessions)
+btn_frame.grid(row=1, column=0, columnspan=4, pady=5, sticky=tk.EW)  # span across all columns
 
-def mark_as_unpaid():
-    # similar to mark_as_paid, but set is_paid=0
-    selected_items = sessions_tree.selection()
-    if not selected_items:
-        return
-    student_name, session_date, _, _ = sessions_tree.item(selected_items[0])['values']
-    session_date_db_format = datetime.strptime(session_date, '%d-%m-%Y').strftime('%Y-%m-%d')  # Convert to database format
-    
-    conn = sqlite3.connect('tutoring_app.db')
-    cursor = conn.cursor()
-    
-    cursor.execute("UPDATE TutoringSessions SET is_paid=0 WHERE student_id = (SELECT id FROM Students WHERE name = ?) AND date_of_session = ?", (student_name, session_date_db_format))
-    conn.commit()
-    conn.close()
-    
-    refresh_sessions_list()
+# Button placement using pack
+btn_mark_paid = ttk.Button(btn_frame, text="Mark as Paid", command=mark_as_paid)
+btn_mark_paid.pack(side=tk.LEFT, padx=(0, 2.5))  # Half padding on the right
 
-# Add buttons for marking sessions as paid or unpaid
-btn_mark_paid = ttk.Button(frame_sessions, text="Mark as Paid", command=mark_as_paid)
-btn_mark_paid.pack(pady=5, side=tk.LEFT, padx=5)
+btn_mark_unpaid = ttk.Button(btn_frame, text="Mark as Unpaid", command=mark_as_unpaid)
+btn_mark_unpaid.pack(side=tk.LEFT, padx=(2.5, 5))  # Half padding on the left
 
-btn_mark_unpaid = ttk.Button(frame_sessions, text="Mark as Unpaid", command=mark_as_unpaid)
-btn_mark_unpaid.pack(pady=5, side=tk.LEFT, padx=5)
+# Spacer frame to push the next buttons to the right
+spacer_frame = ttk.Frame(btn_frame)
+spacer_frame.pack(side=tk.LEFT, fill=tk.X, expand=True)  # Expand to occupy available space
 
+btn_edit_session = ttk.Button(btn_frame, text="Edit Session", command=edit_session)
+btn_edit_session.pack(side=tk.LEFT, padx=(0, 2.5))  # Half padding on the right
 
-btn_edit_session = ttk.Button(frame_sessions, text="Edit Session", command=edit_session)
-btn_edit_session.pack(pady=5, side=tk.LEFT, padx=5)
-
-btn_delete_session = ttk.Button(frame_sessions, text="Delete Session", command=delete_session)
-btn_delete_session.pack(pady=5, side=tk.LEFT, padx=5)
+btn_delete_session = ttk.Button(btn_frame, text="Delete Session", command=delete_session)
+btn_delete_session.pack(side=tk.LEFT, padx=(2.5, 0))  # Half padding on the left
 
 # Frame for CRUD operations on students
 frame_crud_students = ttk.LabelFrame(root, text="Manage Students", padding=(10, 5))
 frame_crud_students.grid(row=2, column=0, padx=10, pady=5, sticky="nsew")
 
-# Widgets for CRUD operations on students
-lbl_name = ttk.Label(frame_crud_students, width=label_width, text="Name:")
-lbl_name.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+entry_frame = ttk.Frame(frame_crud_students)
+entry_frame.pack(side=tk.LEFT, fill=tk.Y, padx=5, pady=5)
 
-name_entry = ttk.Entry(frame_crud_students, width=entry_width)
-name_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+# Name label and entry
+name_frame = ttk.Frame(entry_frame)
+name_frame.pack(fill=tk.X, pady=2)
+lbl_name = ttk.Label(name_frame, text="Name:", width=label_width)
+lbl_name.grid(row=0, column=0, sticky=tk.W)
+name_entry = ttk.Entry(name_frame, width=entry_width)
+name_entry.grid(row=0, column=1, sticky=tk.W)
 
-lbl_hourly_rate = ttk.Label(frame_crud_students, width=label_width, text="Hourly Rate:")
-lbl_hourly_rate.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+# Hourly rate label and entry
+hourly_rate_frame = ttk.Frame(entry_frame)
+hourly_rate_frame.pack(fill=tk.X, pady=2)
+lbl_hourly_rate = ttk.Label(hourly_rate_frame, text="Hourly Rate:", width=label_width)
+lbl_hourly_rate.grid(row=0, column=0, sticky=tk.W)
+hourly_rate_entry = ttk.Entry(hourly_rate_frame, width=entry_width)
+hourly_rate_entry.grid(row=0, column=1, sticky=tk.W)
 
-hourly_rate_entry = ttk.Entry(frame_crud_students, width=entry_width)
-hourly_rate_entry.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+# Spacer between entries and buttons
+spacer = ttk.Label(entry_frame)  # Adjust height as needed
+spacer.pack(fill=tk.X, pady=33)
 
-btn_add_student = ttk.Button(frame_crud_students, text="Add Student", command=add_student)
-btn_add_student.grid(row=2, column=0, columnspan=2, pady=10, sticky=tk.W)
+btn_frame = ttk.Frame(entry_frame)
+btn_frame.pack(fill=tk.X, pady=5)
 
-tree = ttk.Treeview(frame_crud_students, columns=('Name', 'Hourly Rate'), show='headings')
+btn_add_student = ttk.Button(btn_frame, text="Add Student", command=add_student)
+btn_add_student.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
+
+btn_edit_student = ttk.Button(btn_frame, text="Edit Student", command=edit_student)
+btn_edit_student.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
+
+btn_delete_student = ttk.Button(btn_frame, text="Delete Student", command=delete_student)
+btn_delete_student.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+# Adjust the height of the tree to fit the height of the two entry widgets
+tree_frame = ttk.Frame(frame_crud_students)
+tree_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+# Setting the height to a specific number of rows
+tree = ttk.Treeview(tree_frame, columns=('Name', 'Hourly Rate'), show='headings', height=6)  # Set height as needed
 tree.heading('Name', text='Name')
 tree.heading('Hourly Rate', text='Hourly Rate')
-tree.grid(row=4, column=0, columnspan=2, padx=5, pady=5, sticky='nsew')
-frame_crud_students.grid_columnconfigure(1, weight=1)  # This allows the Treeview to expand horizontally
-frame_crud_students.grid_rowconfigure(4, weight=1)  # This allows the Treeview to expand vertically
+tree.pack(fill=tk.BOTH, expand=True, padx=5)
 
 refresh_students_list()
-
-btn_edit_student = ttk.Button(frame_crud_students, text="Edit Student", command=edit_student)
-btn_edit_student.grid(row=5, column=0, padx=5, pady=5, sticky='ew')
-
-btn_delete_student = ttk.Button(frame_crud_students, text="Delete Student", command=delete_student)
-btn_delete_student.grid(row=5, column=1, padx=5, pady=5, sticky='ew')
 
 # Create a statistics frame to the right
 stats_frame = ttk.LabelFrame(root, text="Statistics", padding=(10, 5))
@@ -426,38 +459,38 @@ stats_frame.grid(row=0, column=1, rowspan=3, padx=10, pady=5, sticky="nsew")
 inner_stats_frame = ttk.Frame(stats_frame)
 inner_stats_frame.pack(fill=tk.BOTH, expand=True)
 
-
 export_button = ttk.Button(stats_frame, text="Export to Excel", command=export_to_excel)
 export_button.pack(side=tk.BOTTOM, pady=10, fill=tk.X)
 
-
 def compute_total_earned_by_student():
+    
     conn = sqlite3.connect('tutoring_app.db')
     cursor = conn.cursor()
+    
+    # Fetch the current month and year
+    current_month = datetime.now().month
+    current_year = datetime.now().year
 
-    # Compute total money earned by each student using JOIN operation
+    # Compute money earned by each student for the current month using JOIN operation
     query = """
     SELECT Students.name, SUM(TutoringSessions.hours_worked * Students.price_per_hour)
     FROM Students
     JOIN TutoringSessions ON TutoringSessions.student_id = Students.id
+    WHERE strftime('%m', date_of_session) = ? AND strftime('%Y', date_of_session) = ?
     GROUP BY Students.name
     """
     
-    cursor.execute(query)
+    cursor.execute(query, (str(current_month).zfill(2), str(current_year)))
     earnings = cursor.fetchall()
     
     # Display earnings in stats_frame
     for idx, (student, amount) in enumerate(earnings):
-        ttk.Label(inner_stats_frame, text=f"{student}: ${amount:.2f}").grid(row=idx, column=0, padx=5, pady=5, sticky=tk.W)
+        ttk.Label(inner_stats_frame, text=f"{student}: {amount:.2f} €").grid(row=idx, column=0, padx=5, pady=5, sticky=tk.W)
 
-    
     conn.close()
     return earnings
 
-earnings = compute_total_earned_by_student()
-
-
-def display_monthly_earnings():
+def display_current_monthly_earnings():
     conn = sqlite3.connect('tutoring_app.db')
     cursor = conn.cursor()
 
@@ -475,12 +508,11 @@ def display_monthly_earnings():
     cursor.execute(query, (str(current_month).zfill(2), str(current_year)))
     total = cursor.fetchone()[0]
     
+    earnings = compute_total_earned_by_student()
     # Display total earnings for the month in stats_frame
-    ttk.Label(inner_stats_frame, text=f"Total for {current_month}/{current_year}: ${total if total else 0:.2f}").grid(row=len(earnings), column=0, padx=5, pady=5, sticky=tk.W)
+    ttk.Label(inner_stats_frame, text=f"Total for {current_month}/{current_year}: {total if total else 0:.2f} €").grid(row=len(earnings), column=0, padx=5, pady=5, sticky=tk.W)
     
     conn.close()
-
-display_monthly_earnings()
 
 def compute_monthly_earnings():
     conn = sqlite3.connect('tutoring_app.db')
@@ -499,17 +531,17 @@ def compute_monthly_earnings():
     monthly_earnings = cursor.fetchall()
     
     conn.close()
-    print(monthly_earnings)
     return monthly_earnings
 
 def display_monthly_earnings():
+
     earnings = compute_monthly_earnings()
     
     # Assuming you have a label or some widget where you display statistics
-    for idx, (month_year, amount) in enumerate(earnings, start=len(earnings) + 3):  # Adjust the start index accordingly
-        ttk.Label(inner_stats_frame, text=f"{month_year}: ${amount:.2f}").grid(row=idx, column=0, padx=5, pady=5, sticky=tk.W)
+    for idx, (month_year, amount) in enumerate(earnings, start=len(earnings) + 4):  # Adjust the start index accordingly
+        ttk.Label(inner_stats_frame, text=f"{month_year}: {amount:.2f} €").grid(row=idx, column=0, padx=5, pady=5, sticky=tk.W)
 
-def compute_total_earned_this_year():
+def compute_total_earned_this_year():   
     conn = sqlite3.connect('tutoring_app.db')
     cursor = conn.cursor()
 
@@ -525,13 +557,14 @@ def compute_total_earned_this_year():
     
     cursor.execute(query, (str(current_year),))
     total = cursor.fetchone()[0]
-    
+
+    # Calculate the correct row index after monthly earnings are displayed
+    monthly_earnings = compute_monthly_earnings()
+    earnings = compute_total_earned_by_student()
     # Display total earnings for the year in stats_frame
-    ttk.Label(inner_stats_frame, text=f"Total for {current_year}: ${total if total else 0:.2f}").grid(row=len(earnings) + 1, column=0, padx=5, pady=5, sticky=tk.W)
+    ttk.Label(inner_stats_frame, text=f"Total for {current_year}: {total if total else 0:.2f} €").grid(row=len(earnings) + len(monthly_earnings) + 1, column=0, padx=5, pady=5, sticky=tk.W)
     
     conn.close()
-
-compute_total_earned_this_year()
 
 def compute_highest_earning_month():
     try:
@@ -555,15 +588,27 @@ def compute_highest_earning_month():
         month, total = cursor.fetchone()
         
         # Display month with highest earnings for the year in stats_frame
-        #   ttk.Label(inner_stats_frame, text=f"Highest in {month}/{current_year}: ${total if total else 0:.2f}").grid(row=len(earnings) + 2, column=0, padx=5, pady=5, sticky=tk.W)
+        #   ttk.Label(inner_stats_frame, text=f"Highest in {month}/{current_year}: {total if total else 0:.2f} €").grid(row=len(earnings) + 2, column=0, padx=5, pady=5, sticky=tk.W)
         conn.close()
     except:
         pass
 
-compute_highest_earning_month()
-display_monthly_earnings()
+def clear_statistics():
+    """Clear all labels in the inner_stats_frame."""
+    for widget in inner_stats_frame.winfo_children():
+        widget.destroy()
 
+def update_statistics():
+    # Clear statistics once at the start
+    clear_statistics()
 
-refresh_sessions_list()
+    # Call the functions in the correct order
+    earnings = compute_total_earned_by_student()
+    display_current_monthly_earnings()
+    display_monthly_earnings()  # This should come before the yearly total now
+    compute_highest_earning_month()
+    compute_total_earned_this_year()
+
+update_statistics()
 
 root.mainloop()
